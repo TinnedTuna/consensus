aa4eba8141f8fdfdcc44033567b38d25ee886c59
Dan Turner <dan.turner@york.ac.uk>
2013-03-18 21:55:41 +0000

    One failing test for viewing a single election.

--- consensus/consensus/tests/test_elections.py
+++ consensus/consensus/tests/test_elections.py
@@ -23,6 +23,7 @@ from consensus.views.user import (
 from consensus.views.elections import (
     create_election,
     view_all_elections,
+    view_election,
     )
 
 from consensus.authentication import (
@@ -59,7 +60,7 @@ class TestElection(unittest.TestCase):
     def test_create_election_no_auth(self):
         request = testing.DummyRequest()
         response = create_election(request)
-        self.assertEqual(response.status_int, 403)
+        self.assertEqual(response.status_int, 401)
         
 
     def test_create_election(self):
@@ -100,6 +101,36 @@ class TestElection(unittest.TestCase):
         request.session = auth_session
         request.POST = MultiDict()
         response = view_all_elections(request)
+        self.assertEqual(len(response), 1)
         self.assertEqual(response['Test']['name'], 'Test')
-        self.assertEqual(response['Test']['body'], 'An election for testing.')
-        self.assertEqual(response['Test']['method']['name'], 'Test Method')
+
+    def test_view_elections(self):
+        request = testing.DummyRequest() 
+        request.POST = MultiDict()
+        request.POST['username'] = 'TestUser'
+        request.POST['password'] = 'TestPass'
+        response = auth(request)
+        auth_session = request.session
+        self.assertTrue(auth_session['authentication'].is_authenticated())
+        request = testing.DummyRequest() 
+        request.session = auth_session
+        request.POST = MultiDict()
+        request.POST['name'] = 'Test'
+        request.POST['body'] = 'An election for testing.'
+        request.POST['method'] = 'TestMethod'
+        response = create_election(request)
+        self.assertEqual(response.status_int, 302)
+        request = testing.DummyRequest() 
+        request.session = auth_session
+        request.POST = MultiDict()
+        response = view_all_elections(request)
+        self.assertEqual(len(response), 1)
+        self.assertEqual(response['Test']['name'], 'Test')
+        request = testing.DummyRequest() 
+        request.session = auth_session
+        request.POST = MultiDict()
+        request.matchdict['id'] = response['Test']['id']
+        response = view_election(request)
+        self.assertEqual(response['name'], 'Test')
+        self.assertEqual(response['body'], 'An election for testing.')
+        self.assertEqual(response['method']['name'], 'Test Method')
